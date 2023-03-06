45600eb3255202b828b8d22b734fa4ab0e38302a
Siven26 <sikabg26@gmail.com>
2023-03-06 19:40:00 +0200

    first commit

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Joke
+    {
+        public int JokeId { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set;}
+        public string JokeType { get; set; }
+    }
+}
